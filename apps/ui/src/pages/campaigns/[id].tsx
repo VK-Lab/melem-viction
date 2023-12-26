5ca2099dc857666165db607b09f6c4a9c2742656
liam <hoailinh1210@gmail.com>
2023-12-26 16:02:13 +0700

    feat: ssr for campaign card minter

--- apps/ui/src/pages/campaigns/[id].tsx
+++ apps/ui/src/pages/campaigns/[id].tsx
@@ -1,24 +1,28 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { dehydrate } from 'react-query';
 
+import { prefetchGetCampaign } from '@/hooks/queries/useGetCampaign';
 import MintCampaign from '@/modules/MintCampaign';
 
 const Index = () => {
   return <MintCampaign />;
 };
 
-export const getStaticProps = async ({ locale }: { locale: string }) => {
+export const getServerSideProps = async ({
+  locale,
+  params,
+}: {
+  locale: string;
+  params: { id: string };
+}) => {
+  const queryClient = await prefetchGetCampaign(params.id);
+
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common'])),
+      dehydratedState: dehydrate(queryClient),
     },
   };
 };
-
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: true,
-  };
-}
 
 export default Index;
